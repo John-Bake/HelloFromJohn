ca17ebd4453fdd77190306000f96b2339dfd5220
Ricky <72452852+rnunez22@users.noreply.github.com>
2020-10-09 19:17:34 -0600

    //Here is Ricardo's first commit

--- Main.java
+++ Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is John");
+
+        //Line from Ricardo
+        System.out.println("Hello John, my name is Ricardo.");
     }
 }
